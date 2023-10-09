5c61dac06b8e1ead39aa0f36b6c84ab39d1f3f67
RWTHLHK <linghao.kongrwth@gmail.com>
2023-10-09 16:00:26 +0200

    successfully read configs.ini

---
+++ python/main.py
@@ -0,0 +1,30 @@
+import os
+import configparser
+
+if __name__ == "__main__":
+  config = configparser.ConfigParser()
+  config.read('./configs/configs.ini')
+
+  # batch_options = config.options('BatchSettings')
+  # cmd = 'salloc'
+  # for opt in batch_options:
+  #   cmd += f" --{opt}={config.get('BatchSettings', opt)}"
+
+  conda_options = config.options('CondaSettings')
+  with open("./configs/jobConfig.sh",'w+') as jc:
+    jc.write("export")
+    for opt in conda_options:
+      jc.write(f' {opt}={config.get("CondaSettings", opt)}')
+
+    jc.write("\n")
+
+  job_options = config.options('JobSettings')
+  with open("./configs/jobConfig.sh",'a+') as jc:
+    jc.write("export")
+    for opt in job_options:
+      jc.write(f' {opt}={config.get("JobSettings", opt)}')
+
+  cmd = "sbatch runBatch.sh"
+  print(cmd)
+  os.system(cmd)
+
--- runBatch.sh
+++ runBatch.sh
@@ -1,14 +1,15 @@
 #!/usr/local_rwth/bin/zsh
 
 # Read user variables
-. configs/jobConfig.sh
-. $CONDA_ROOT/etc/profile.d/conda.sh
-export PATH="$CONDA_ROOT/bin:$PATH"
+source configs/jobConfig.sh
+echo $conda_root
+. $conda_root/etc/profile.d/conda.sh
+export PATH="$conda_root/bin:$PATH"
 # Now you can activate your configured conda environments
-conda activate $CONDA_ENV
+conda activate $conda_env
 ### Execute your application
 ### Please remember, to adjust the memory, it must be less than requested above
 
 ### For debugging everything above you can simply echo the command instead of running the python script
 #echo $PYTHONCODE -t $TEST_FLAG -r $RESTART_FLAG -s $SIM_TYP -n $SLURM_NTASKS -j $SIM_JOB_BASE_NAME -d $PWD --ex_data=$EX_DATA
-python $PYTHONCODE -t $TEST_FLAG -r $RESTART_FLAG -s $SIM_TYP -n $SLURM_NTASKS -j $SIM_JOB_BASE_NAME -d $PWD --ex_data=$EX_DATA --phases=$PHASES
+python $pythoncode -t $test_flag -r $restart_flag -s $sim_type -n $slurm_ntasks -j $sim_job_base_name -d $PWD --ex_data=$ex_data --phases=$phases
