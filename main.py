6d80de1eb75094b246ff062a2f56e7a29b14d615
mhenrich1990 <m.henrichv@googlemail.com>
2023-10-26 18:32:07 +0200

    make log and image folder variable to job-name

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ condaRoot = condaSettings['CONDA_ROOT']
 condaEnv = condaSettings['CONDA_ENV']
 pythoncode = config.get('JobSettings','PYTHONCODE')
 
+sim_job_base_name = config.get('JobSettings','SIM_JOB_BASE_NAME')
+
 f = open('./configs/JobConfig.sh', 'w+')
 f.write(f'CONDA_ROOT={condaRoot}\n')
 f.write(f'CONDA_ENV={condaEnv}\n')
@@ -26,18 +28,18 @@ f.write(f'PYTHONCODE={pythoncode}\n')
 f.write(f'ROOT={os.getcwd()}')
 f.close()
 
-image_path = f'{os.getcwd()}/evaluation_images'
-logs_path = f'{os.getcwd()}/logs'
+image_path = f'{os.getcwd()}/evaluation_images_{sim_job_base_name}'
+logs_path = f'{os.getcwd()}/logs_{sim_job_base_name}'
 
 if os.path.isdir(image_path) and len(os.listdir(image_path)) > 0:
-        os.system(f'rm -r {os.getcwd()}/evaluation_images/*.png')
+        os.system(f'rm -r {image_path}/*.png')
 
 if not os.path.isdir(logs_path):
         os.mkdir(f'{os.getcwd()}/logs')
 elif os.path.isdir(logs_path) and len(os.listdir(logs_path)) > 0:
-        os.system(f'rm -r {os.getcwd()}/logs/*log*')
-        files = glob.glob(f"{os.getcwd()}/logs/*.txt")
+        os.system(f'rm -r {logs_path}/*log*')
+        files = glob.glob(f"{logs_path}/*.txt")
         if files:
-                os.system(f'rm -r {os.getcwd()}/logs/*txt')
+                os.system(f'rm -r {logs_path}/*txt')
 # run batch file
 os.system(f"sbatch --job-name={jobName} --output={output} --time={time} --mem-per-cpu={memPerCpu} --nodes={nodes} --ntasks={ntasks} runBatch.sh")
