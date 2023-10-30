056c2146f4a3f18b086304a3def1c760297db359
RWTHLHK <linghao.kongrwth@gmail.com>
2023-10-30 10:57:07 +0100

    test damask config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import configparser
 import glob
+import sys
 
 config = configparser.ConfigParser()
 config.read('./configs/configs.ini')
@@ -12,32 +13,39 @@ time = batchSettings['time']
 memPerCpu = batchSettings['mem-per-cpu']
 nodes = batchSettings['nodes']
 ntasks = batchSettings['ntasks']
+software = batchSettings['software']
 
 #call conda settings
 condaSettings = config['CondaSettings']
 condaRoot = condaSettings['CONDA_ROOT']
 condaEnv = condaSettings['CONDA_ENV']
-pythoncode = config.get('JobSettings','PYTHONCODE')
+if software == "abaqus":
+        pythoncode = config.get('AbaqusJobSettings','PYTHONCODE')
+elif software == "damask":
+        pythoncode = config.get('DamaskJobSettings','PYTHONCODE')
+else:
+        os.system("echo error: wrong software name!")
+        sys.exit()
 
-f = open('./configs/JobConfig.sh', 'w+')
-f.write(f'CONDA_ROOT={condaRoot}\n')
-f.write(f'CONDA_ENV={condaEnv}\n')
-f.write(f'PYTHONCODE={pythoncode}\n')
-f.write(f'ROOT={os.getcwd()}')
-f.close()
+# f = open('./configs/JobConfig.sh', 'w+')
+# f.write(f'CONDA_ROOT={condaRoot}\n')
+# f.write(f'CONDA_ENV={condaEnv}\n')
+# f.write(f'PYTHONCODE={pythoncode}\n')
+# f.write(f'ROOT={os.getcwd()}')
+# f.close()
 
-image_path = f'{os.getcwd()}/evaluation_images'
-logs_path = f'{os.getcwd()}/logs'
+# image_path = f'{os.getcwd()}/evaluation_images'
+# logs_path = f'{os.getcwd()}/logs'
 
-if os.path.isdir(image_path) and len(os.listdir(image_path)) > 0:
-        os.system(f'rm -r {os.getcwd()}/evaluation_images/*.png')
+# if os.path.isdir(image_path) and len(os.listdir(image_path)) > 0:
+#         os.system(f'rm -r {os.getcwd()}/evaluation_images/*.png')
 
-if not os.path.isdir(logs_path):
-        os.mkdir(f'{os.getcwd()}/logs')
-elif os.path.isdir(logs_path) and len(os.listdir(logs_path)) > 0:
-        os.system(f'rm -r {os.getcwd()}/logs/*log*')
-        files = glob.glob(f"{os.getcwd()}/logs/*.txt")
-        if files:
-                os.system(f'rm -r {os.getcwd()}/logs/*txt')
-# run batch file
-os.system(f"sbatch --job-name={jobName} --output={output} --time={time} --mem-per-cpu={memPerCpu} --nodes={nodes} --ntasks={ntasks} runBatch.sh")
+# if not os.path.isdir(logs_path):
+#         os.mkdir(f'{os.getcwd()}/logs')
+# elif os.path.isdir(logs_path) and len(os.listdir(logs_path)) > 0:
+#         os.system(f'rm -r {os.getcwd()}/logs/*log*')
+#         files = glob.glob(f"{os.getcwd()}/logs/*.txt")
+#         if files:
+#                 os.system(f'rm -r {os.getcwd()}/logs/*txt')
+# # run batch file
+# os.system(f"sbatch --job-name={jobName} --output={output} --time={time} --mem-per-cpu={memPerCpu} --nodes={nodes} --ntasks={ntasks} runBatch.sh")
